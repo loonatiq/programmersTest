f428a9e0f1462d7407eb7fa6350cc38d3eb56375
loonatiq <86408147+loonatiq@users.noreply.github.com>
2022-03-16 23:47:20 +0900

    220316

--- 42578.js
+++ 42578.js
@@ -42,20 +42,32 @@ const clothes = [
   ["bluesunglasses", "eyewear"],
   ["green_turban", "headgear"],
 ];
-function solution(clothes) {
-  let result = {};
-  clothes.forEach((value) => {
-    result[value[1]] = (result[value[1]] || 0) + 1;
-  });
-  let num = Object.values(result).map((value) => value + 1);
+// function solution(clothes) {
+//   let result = {};
+//   clothes.forEach((value) => {
+//     result[value[1]] = (result[value[1]] || 0) + 1;
+//   });
+//   let num = Object.values(result).map((value) => value + 1);
 
-  console.log(num);
-  let mult = 1;
-  num.forEach((value) => {
-    mult *= value;
-  });
-  console.log(mult - 1);
-  return mult - 1;
+//   console.log(num);
+//   let mult = 1;
+//   num.forEach((value) => {
+//     mult *= value;
+//   });
+//   console.log(mult - 1);
+//   return mult - 1;
+// }
+
+function solution(clothes) {
+  return (
+    Object.values(
+      clothes.reduce((obj, t) => {
+        obj[t[1]] = obj[t[1]] ? obj[t[1]] + 1 : 1;
+        console.log(obj);
+        return obj;
+      }, {})
+    ).reduce((a, b) => a * (b + 1), 1) - 1
+  );
 }
 
-solution(clothes);
+console.log(solution(clothes));
